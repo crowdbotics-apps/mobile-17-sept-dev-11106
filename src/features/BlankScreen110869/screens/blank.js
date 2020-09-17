0ba08a2aabd6875d8dee031e23eb019e61a765f4
Crowdbotics <team@crowdbotics.com>
2020-09-17 08:49:08 +0000

    Update components

--- src/features/BlankScreen110869/screens/blank.js
+++ src/features/BlankScreen110869/screens/blank.js
@@ -22,7 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { CheckBox_3: true, Radio_4: true }
 
   render = () => (
     <View
@@ -66,7 +66,7 @@ export class _Blank extends React.Component {
           fontFamily: "Roboto-Regular"
         }}
         style={{
-          width: "100%",
+          width: 100,
           marginLeft: 0,
           marginRight: 0,
           marginTop: 5,
@@ -93,12 +93,173 @@ export class _Blank extends React.Component {
           textDecorationLine: "none",
           textTransform: "none",
           lineHeight: 12,
-          letterSpacing: 0
+          letterSpacing: 0,
+          alignSelf: "center"
         }}
         onPress={() => alert("Pressed!")}
       >
         Press me!
       </Button>
+      <CheckBox
+        text="Checkbox"
+        disabled={false}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          alignSelf: "center",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textAlign: "left",
+          textAlignVertical: "center",
+          textDecorationLine: "none",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
+        }}
+        checked={this.state.CheckBox_3}
+        onChange={nextChecked => this.setState({ CheckBox_3: nextChecked })}
+      />
+      <Radio
+        text="Radio button"
+        disabled={false}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          alignSelf: "center",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textAlign: "left",
+          textAlignVertical: "center",
+          textDecorationLine: "none",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
+        }}
+        checked={this.state.Radio_4}
+        onChange={nextChecked => this.setState({ Radio_4: nextChecked })}
+      />
+      <Icon
+        iconFont="Eva Design Icons"
+        name="alert-triangle"
+        height={40}
+        width={40}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          alignSelf: "center",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          fontSize: 40,
+          color: "#f10e0e",
+          backgroundColor: "#fcfcfc",
+          fontFamily: "Roboto-Regular",
+          textAlign: "left",
+          textAlignVertical: "center",
+          textDecorationLine: "line-through",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
+        }}
+      />
+      <Icon
+        iconFont="Eva Design Icons"
+        name="star"
+        height={20}
+        width={20}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          alignSelf: "center",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontFamily: "Roboto-Regular",
+          textAlign: "left",
+          textAlignVertical: "center",
+          textDecorationLine: "none",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
+        }}
+      />
+      <Image
+        resizeMode="cover"
+        style={{
+          width: "100%",
+          height: 200,
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          overflow: "visible",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0
+        }}
+      />
     </View>
   )
 }
